25e1cfff7b0270514d5c12414a6a04c40a60873e
Deminq11 <demian1541@gmail.com>
2025-03-07 18:59:53 -0300

    debuggin for railway

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import './index.css'
 import UserProvider from './context/UserProvider.jsx';
 
-console.log("hola")
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <UserProvider>
--- frontend/src/utils/readProducts.js
+++ frontend/src/utils/readProducts.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 export default async function readProducts(id){
     try{
+        console.log(import.meta.env.VITE_API_URL)
         const url = id ? `${import.meta.env.VITE_API_URL}/api/products/${id}` : `${import.meta.env.VITE_API_URL}/api/products/`
         const {data} = await axios.get(url)
         return data
