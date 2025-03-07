c77a06bd0523a63179d2ce9e9ebad39b2ff1fa7d
Deminq11 <demian1541@gmail.com>
2025-03-07 16:27:07 -0300

    configuration for railway deployment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import errorHandler from './middlewares/errorHandler.mid.js';
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import morgan from "morgan";
+import dbConnect from "./utils/dbConnect.js"
 
 const app=express();
 
@@ -22,4 +23,10 @@ app.use("/", indexRouter)
 app.use(pathHandler)
 app.use(errorHandler)
 
+const PORT=process.env.PORT
+const ready = ()=> {
+    console.log(`Server escuchando en puerto ${PORT}`)
+    dbConnect()
+}
+app.listen(PORT, ready)
 export default app
